25c01ee5f913b3061952f14f45dc0c9618121e76
Losstarot09 <lobanovs.arturs@gmail.com>
2021-08-18 11:59:44 +0300

    Delete ProgForCognizant/testSBv2312 directory

--- ProgForCognizant/testSBv2312/src/main/java/com/taskSBv2312/testSBv2312/repository/SubTaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taskSBv2312.testSBv2312.repository;
-
-import com.taskSBv2312.testSBv2312.model.Subtask;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SubTaskRepository extends CrudRepository <Subtask, Long> {
-}
--- ProgForCognizant/testSBv2312/src/main/java/com/taskSBv2312/testSBv2312/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.taskSBv2312.testSBv2312.repository;
-
-import com.taskSBv2312.testSBv2312.model.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository <Task, Long> {
-
-}
